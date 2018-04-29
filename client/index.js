2de57626f883be7d4f0d8887b47e2a69448a4f2e
adsznzhang <adszn@hotmail.com>
2018-04-29 20:51:47 +1200

    check current user

--- client/index.js
+++ client/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//apolloclient不知道react，它需要apolloprovider来粘合react
+import ApolloClient from 'apollo-client';
+import {ApolloProvider} from 'react-apollo';
+import {Router, hashHistory, Route,IndexRoute} from 'react-router';
+import App from './components/App.js'
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,14 @@
 const graphql = require('graphql');
 const { GraphQLObjectType , GraphQLID} = graphql;
+const UserType = require('./user_type.js');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields:{
-    dummyField:{type: GraphQLID}
+      type: UserType,
+      resolve(parentValue,args,req){
+          return req.user;
+      }
   }
 });
 
