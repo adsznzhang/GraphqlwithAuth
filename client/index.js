5af766235d10fd5f5a583ddabd09d493eec086b3
adsznzhang <adszn@hotmail.com>
2018-04-30 21:31:47 +1200

    Find current user

--- client/index.js
+++ client/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //apolloclient不知道react，它需要apolloprovider来粘合react
-import ApolloClient from 'apollo-client';
+import ApolloClient,{createNetworkInterface} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 import {Router, hashHistory, Route,IndexRoute} from 'react-router';
 import App from './components/App.js'
 
+//让浏览器到后端的请求默认带cookies
+const networkInterface = createNetworkInterface({
+  uri:'/graphql',
+  opts:{
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -5,10 +5,12 @@ const UserType = require('./user_type.js');
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields:{
+    user:{
       type: UserType,
       resolve(parentValue,args,req){
-          return req.user;
+        return req.user;
       }
+    }
   }
 });
 
