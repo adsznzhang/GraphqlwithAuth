9c1e26732b64d330926a5e772cab283b16783bdc
adsznzhang <adszn@hotmail.com>
2018-05-04 21:18:27 +1200

    Add logout logic

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,15 +2,26 @@ import React ,{Component}from 'react';
 import {Link} from 'react-router';
 import {graphql} from 'react-apollo';
 import query from '../queries/CurrentUser.js';
+import mutation from '../mutations/Logout.js';
 
 class Header extends Component{
+
+
+  onLogoutClick(){
+    this.props.mutate({
+      refetchQueries:[{query}]
+    });
+  }
+
   renderButtons(){
     const {loading, user} = this.props.data;
 
     if(loading) {return <div />;}
 
     if(user){
-      return <div>Logout</div>;
+      return (
+
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>);
     }else{
       return (
         <div>
@@ -41,4 +52,4 @@ class Header extends Component{
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(graphql(query)(Header));
